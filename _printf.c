c8a772b23023e3fb61a3bd4884ecfebee9cc6f14
tenyapious <tenyapious@gmail.com>
2023-08-19 21:48:14 +0000

    declare a va_list variable

--- _printf.c
+++ _printf.c
@@ -4,5 +4,19 @@
 
 int _printf(const char *format, ...)
 {
+	va_list args;
+
+	va_start(args, format);
+
+// loop through the characters of format
+	// if a char == %
+		// check the char after %
+		// if that char is c
+			// call a fn to handle it
+		// else
+			// print the char as it is
+
+	va_end(args);
+
 	return (0)
 }
