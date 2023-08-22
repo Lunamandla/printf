061b0aa77069d22e756302c037a97c3536bf97e9
Lunamandla <lunamandlaluna@gmail.com>
2023-08-22 08:27:06 -0700

    Fixing bettty errors from fmt_function.c and main.h files

--- get_fmt_fn.c
+++ get_fmt_fn.c
@@ -2,8 +2,17 @@
 #include <stdarg.h>
 #include "main.h"
 
+/**
+ * get_fmt_fn - function that gets function pointers
+ * @c: character
+ * Return: value of get format function
+ */
+
 int (*get_fmt_fn(char c))(va_list)
 {
+/**
+ * struct fmt_fn - user-defined datatypes that group different datatypes
+ */
 	struct fmt_fn fmt_fn_arr[] = {
 		{'c', print_char},
 		{'s', print_string},
@@ -11,7 +20,7 @@ int (*get_fmt_fn(char c))(va_list)
 		{' ', print_nothing},
 		{'\0', print_nothing},
 		{0, NULL}
-	};
+	} fmt_fn;
 
 	int i = 0;
 
