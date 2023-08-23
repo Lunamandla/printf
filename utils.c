77eefb87fc42bbe28529013bccbe1baa9302f02a
Lunamandla <lunamandlaluna@gmail.com>
2023-08-23 00:15:34 -0700

    Adding Description for utils.c file

--- utils.c
+++ utils.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include "main.h"
 
+/**
+ * get_int_len - function to get the length of the integer
+ * @n: number of charcaters
+ *
+ * Return: value
+ */
 int get_int_len(int n)
 {
 	int i = 0;
@@ -13,6 +19,12 @@ int get_int_len(int n)
 	return (++i);
 }
 
+/**
+ * is_negative - function to check if the number are negetive
+ * @n: pointer to function
+ * @print_count: pointer that count characters
+ * Return: 0
+ */
 int is_negative(int *n, int *print_count)
 {
 	if (*n < 0)
@@ -31,6 +43,11 @@ int is_negative(int *n, int *print_count)
 	return (0);
 }
 
+/**
+ * print_int_min - function that prints minimum
+ * @n: unused attribute
+ * Return: 0
+ */
 int print_int_min(int n __attribute__((unused)))
 {
 	return (0);
